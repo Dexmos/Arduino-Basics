6f1596b72295e3786d961adf28a2227c4942a843
Florian Marie <dexmos.fire@gmail.com>
2020-02-05 21:50:33 +0100

    Add read input button

---
+++ 5-ReadButtonInput/5-ReadButtonInput.ino
@@ -0,0 +1,25 @@
+/*
+ * WorkShop Arduino
+ * Epitech HUB activity
+ * Dexmos
+ */
+
+// Used pin for button (on the arduino)
+int Button = 8;
+
+int ButtonReader;
+
+void setup() {
+  pinMode(Button, INPUT);       // Set 8 pin to current Input
+  digitalWrite(Button, HIGH);   // Set up the button (if you don't the button will return HIGH/LOW/HIGH/LOW.. etc)
+  Serial.begin(9600);           // Set port for the console
+}
+
+void loop() {
+  ButtonReader = digitalRead(Button);   // Read input from Button (HIGH or LOW)
+
+  if (ButtonReader == HIGH)
+    Serial.println("Button HIGH");
+  else //(ButtonReader == LOW)          // Button press
+    Serial.println("Button LOW");
+}
